de12f864ec55d0b8a374e427b320956b8bb792db
Sang Dang <sangdth@gmail.com>
2021-04-05 01:41:41 +0300

    ant can move :D

--- ant.lua
+++ ant.lua
@@ -1,15 +1,23 @@
+
 Ant = {}
+Iter = 0
 
 function Ant:load()
-  self.x      = 50
+  self.x      = 500
   self.y      = love.graphics.getHeight() / 2
   self.width  = 5
   self.height = 5
-  self.speed  = 10
+  self.speed  = 1000
 end
 
 function Ant:update(dt)
-  
+  Iter = Iter + dt
+  self.x = 200 + math.sin(Iter) * 100
+  self.y = 200 + math.sin(Iter / 1.5) * 100
+
+  if self.y < 0 then
+    self.y = 0
+  end
 end
 
 function Ant:draw()
--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-  t.title = "Kien 2"
+  t.title = "Kien"
   t.version = "11.3"
   t.console = true
   t.window.width = 1280
